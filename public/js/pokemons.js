e44b9342ae7b141527587c5e1586395d6dfef0cf
pedrogabriel2002 <bielpedro300@gmail.com>
2022-03-28 11:05:55 -0300

    Small Fixes

--- public/js/pokemons.js
+++ public/js/pokemons.js
@@ -11,14 +11,6 @@ const addYourPokemon = (pokemonName) => {
     return yourPokemons
 }
 
-const showMyPokemons = () => {
-    return myPokemons.length
-}
-
-const showYourPokemons = () => {
-    return yourPokemons.length
-}
-
 const deleteAll = () => {
     myPokemons.splice(0, myPokemons.length)
     yourPokemons.splice(0, yourPokemons.length)
